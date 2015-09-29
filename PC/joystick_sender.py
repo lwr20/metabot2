31298dbbadf1fdec4720a90f486a344c09a68afe
john.palombo@metaswitch.com <john.palombo@metaswitch.com>
2015-09-29 17:06:56 +0100

    Code tidy-up and change to send controller type to RPi

--- PC/joystick_sender.py
+++ PC/joystick_sender.py
@@ -1,25 +1,30 @@
-import pygame, socket, json, struct, sys
+import pygame
+import socket
+import json
+import struct
+import sys
 
-HOST = '10.5.5.1'    # The IP address for the robot
+HOST = '10.5.5.1'  # The IP address for the robot
 PORT = 10000
 
-CONTROLLER = "Wireless Controller"
-
 # Define some colors
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-# This is a simple class that will help us print to the screen
-# It has nothing to do with the joysticks, just outputing the
-# information.
-class TextPrint:
-    def __init__(self):
-        self.reset()
+
+# Class to write Joystick info to the screen.
+class Screen:
+    def __init__(self, _surface):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+
         self.font = pygame.font.Font(None, 20)
+        self.screen = _surface
 
-    def myprint(self, screen, textString):
-        textBitmap = self.font.render(textString, True, BLACK)
-        screen.blit(textBitmap, [self.x, self.y])
+    def myprint(self, textstring):
+        textbitmap = self.font.render(textstring, True, BLACK)
+        self.screen.blit(textbitmap, [self.x, self.y])
         self.y += self.line_height
 
     def reset(self):
@@ -33,179 +38,152 @@ class TextPrint:
     def unindent(self):
         self.x -= 10
 
-class Sender:
-    def __init__(self, host, port):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
-    def send(self, data):
-        payload = json.dumps(data)
-        length = len(payload)
-        header = struct.pack("<L", length)
-        self.socket.sendall("%s%s" % (header,payload))
+    def update(self):
+        self.screen.fill(WHITE)
+        self.reset()
 
+        # Get count of joysticks
+        joystick_count = pygame.joystick.get_count()
 
-# Check for a command line argument
-if len(sys.argv) > 1:
-	host = sys.argv[1]
-else:
-	host = HOST
+        self.myprint("Number of joysticks: {}".format(joystick_count))
+        self.indent()
 
-pygame.init()
+        # For each joystick:
+        for i in range(joystick_count):
+            joystick = pygame.joystick.Joystick(i)
+            joystick.init()
 
-# Set the width and height of the screen [width,height]
-size = [500, 700]
-screen = pygame.display.set_mode(size)
+            self.myprint("Joystick {}".format(i))
+            self.indent()
 
-pygame.display.set_caption("My Game")
+            # Get the name from the OS for the controller/joystick
+            name = joystick.get_name()
+            self.myprint("Joystick name: {}".format(name))
 
-#Loop until the user clicks the close button.
-done = False
-
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
-
-# Initialize the joysticks
-pygame.joystick.init()
-
-# Get ready to print
-textPrint = TextPrint()
-
-# Create our network handler
-sender = Sender(host, PORT)
-
-def send_update(joystick):
-    # create a dictionary to save our joystick data in
-    simple_joy = {}
-
-    # get all the stick data
-    axes = joystick.get_numaxes()
-    axis_list = []
-    for i in range(axes):
-        axis = joystick.get_axis(i)
-        axis_list.append("{:>6.3f}".format(axis))
-    simple_joy["sticks"] = axis_list
-
-    # get all the button data
-    buttons = joystick.get_numbuttons()
-    button_list = []
-    for i in range(buttons):
-        button = joystick.get_button( i )
-        button_list.append("{}".format(button))
-    simple_joy["buttons"]=button_list
-
-    # get all the hat data
-    hat_list = []
-    hats = joystick.get_numhats()
-    for i in range(hats):
-        hat = joystick.get_hat( i )
-        hat_list.append("{}".format(str(hat)))
-    simple_joy["hats"]=hat_list
-
-    # send it off over the network
-    sender.send(simple_joy)
-
-def get_controller(controller_name):
-    """Returns the index of the desired controller"""
-    joystick_count = pygame.joystick.get_count()
-    for i in range(joystick_count):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
-        name = joystick.get_name()
-        if controller_name in name:
-            return i
-    # We didn't find the desired controller
-    return -1
-
-
-
-controller_index = get_controller(CONTROLLER)
-if controller_index == -1:
-    print "Desired controller not found!"
-    sys.exit(1)
-
-# -------- Main Program Loop -----------
-while done==False:
-    # EVENT PROCESSING STEP
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
-        else:
-            send_update(joystick = pygame.joystick.Joystick(controller_index))
-        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        # if event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYBUTTONUP:
-            # print("Joystick button changed.")
-        # elif event.type == pygame.JOYAXISMOTION
-            # print("Joystick button released.")
-
-
-
-    # DRAWING STEP
-    # First, clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-    screen.fill(WHITE)
-    textPrint.reset()
-
-    # Get count of joysticks
-    joystick_count = pygame.joystick.get_count()
-
-    textPrint.myprint(screen, "Number of joysticks: {}".format(joystick_count) )
-    textPrint.indent()
-
-    # For each joystick:
-    for i in range(joystick_count):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
-
-        textPrint.myprint(screen, "Joystick {}".format(i) )
-        textPrint.indent()
-
-        # Get the name from the OS for the controller/joystick
-        name = joystick.get_name()
-        textPrint.myprint(screen, "Joystick name: {}".format(name) )
-
-        # Don't bother with anything which isn't the controller we're
-        # expecting
-        if CONTROLLER in name:
             # Usually axis run in pairs, up/down for one, and left/right for
             # the other.
             axes = joystick.get_numaxes()
-            textPrint.myprint(screen, "Number of axes: {}".format(axes) )
-            textPrint.indent()
-            for i in range(axes):
-                axis = joystick.get_axis(i)
-                textPrint.myprint(screen, "Axis {} value: {:>6.3f}".format(i, axis) )
-            textPrint.unindent()
+            self.myprint("Number of axes: {}".format(axes))
+            self.indent()
+            for j in range(axes):
+                axis = joystick.get_axis(j)
+                self.myprint("Axis {} value: {:>6.3f}".format(j, axis))
+            self.unindent()
 
             buttons = joystick.get_numbuttons()
-            textPrint.myprint(screen, "Number of buttons: {}".format(buttons) )
-            textPrint.indent()
+            self.myprint("Number of buttons: {}".format(buttons))
+            self.indent()
 
-            for i in range(buttons):
-                button = joystick.get_button( i )
-                textPrint.myprint(screen, "Button {:>2} value: {}".format(i,button) )
-            textPrint.unindent()
+            for j in range(buttons):
+                button = joystick.get_button(j)
+                self.myprint("Button {:>2} value: {}".format(j, button))
+            self.unindent()
 
             # Hat switch. All or nothing for direction, not like joysticks.
             # Value comes back in an array.
             hats = joystick.get_numhats()
-            textPrint.myprint(screen, "Number of hats: {}".format(hats) )
-            textPrint.indent()
+            self.myprint("Number of hats: {}".format(hats))
+            self.indent()
 
-            for i in range( hats ):
-                hat = joystick.get_hat( i )
-                textPrint.myprint(screen, "Hat {} value: {}".format(i, str(hat)) )
+            for j in range(hats):
+                hat = joystick.get_hat(i)
+                self.myprint("Hat {} value: {}".format(j, str(hat)))
 
-            textPrint.unindent()
-            textPrint.unindent()
+            self.unindent()
+            self.unindent()
 
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+
+
+class Sender:
+    def __init__(self, _host, port):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((_host, port))
+
+    def send(self, data):
+        payload = json.dumps(data)
+        length = len(payload)
+        header = struct.pack("<L", length)
+        self.socket.sendall("%s%s" % (header, payload))
+
+    def send_update(self, joystick):
+        # create a dictionary and add the controller name
+        simple_joy = {"controller": joystick.get_name()}
+
+        # get all the stick data
+        axes = joystick.get_numaxes()
+        axis_list = []
+        for i in range(axes):
+            axis = joystick.get_axis(i)
+            axis_list.append("{:>6.3f}".format(axis))
+        simple_joy["sticks"] = axis_list
+
+        # get all the button data
+        buttons = joystick.get_numbuttons()
+        button_list = []
+        for i in range(buttons):
+            button = joystick.get_button(i)
+            button_list.append("{}".format(button))
+        simple_joy["buttons"] = button_list
+
+        # get all the hat data
+        hat_list = []
+        hats = joystick.get_numhats()
+        for i in range(hats):
+            hat = joystick.get_hat(i)
+            hat_list.append("{}".format(str(hat)))
+        simple_joy["hats"] = hat_list
+
+        # send it off over the network
+        self.send(simple_joy)
+
+
+# ==============================
+# Main
+
+# Check for a command line argument
+if len(sys.argv) > 1:
+    host = sys.argv[1]
+else:
+    host = HOST
+
+# Create our network handler
+sender = Sender(host, PORT)
+
+# Set-up pygame
+pygame.init()
+surface = pygame.display.set_mode((500, 700))
+pygame.display.set_caption("Metabot2 Controller")
+clock = pygame.time.Clock()
+pygame.joystick.init()
+
+# Set-up pretty printer
+screen = Screen(surface)
+
+controllerEvents = (pygame.JOYAXISMOTION,
+                    pygame.JOYBALLMOTION,
+                    pygame.JOYBUTTONDOWN,
+                    pygame.JOYBUTTONUP,
+                    pygame.JOYHATMOTION)
+
+# -------- Main Program Loop -----------
+# Loop until the user clicks the close button.
+done = False
+while not done:
+
+    # EVENT PROCESSING STEP
+    event = pygame.event.poll()  # Poll for an event
+    if event.type == pygame.QUIT:  # If user clicked close
+        done = True  # Flag that we are done so we exit this loop
+    elif event.type in controllerEvents:
+        sender.send_update(joystick=pygame.joystick.Joystick(event.dict['joy']))
+
+    # Write out info about the currently attached joysticks
+    screen.update()
 
     # Limit to 20 frames per second
     clock.tick(20)
 
-
 # Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-pygame.quit ()
+pygame.quit()
